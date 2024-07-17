a405fbadc943dc70023ce8ee77387a99be32d0ef
guthember <guthember@gmail.com>
2024-07-17 19:36:00 +0200

    code

---
+++ src/main/java/com/guthember/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,28 @@
+package com.guthember.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcomeMessage() {
+        return "Welcome to Github Cicd-Actions";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+//    echo "# github-actions-example" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin git@github.com:guthember/github-actions-example.git
+//    git push -u origin main
+}
